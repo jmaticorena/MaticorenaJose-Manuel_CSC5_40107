9771c49f2201c4ee57b8ec20629b19c11e2c4f05
rcc <rcc>
2017-01-19 11:48:42 -0800

    Signed-off-by: rcc <rcc>

---
+++ Hmwk/Assignment_4/Gaddis_8thEd_Chap5_Prob7_PennyDoubling/main.cpp
@@ -0,0 +1,48 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on January 19, 2017, 10:55 AM
+ * Purpose:  Penny Double
+ */
+
+//System Libraries Here
+#include <iostream>//Input and output library
+#include <cmath>   //power function
+#include <iomanip> //formatting
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    int salary=1,totPay=0,maxDays=31;//Salary and Pay in Pennies
+    //Input or initialize values Here
+    
+    //Process/Calculations Here
+    for(int day=1;day<=maxDays;day++,salary*=2){
+        int cents=salary%100;
+        int dollars=(salary-cents)/100;
+        totPay+=salary;
+        cout<<"Salary for day "<<day<<" = $"<<dollars<<"."
+                <<(cents<10?'0':'\0')<<cents<<endl<<endl;
+        cents=totPay%100;
+        dollars=(totPay-cents)/100;
+        cout<<"Total pay for day "<<day<<" = $"<<dollars<<"."
+                <<(cents<10?'0':'\0')<<cents<<endl<<endl;
+    }
+       //Predicting the Result
+    double salLDay=(pow(2,maxDays-1))/100;
+    double totLDay=(pow(2,maxDays)-1)/100;
+    cout<<fixed<<setprecision(2)<<showpoint;
+    cout<<"Final salary predicted=$"<<salLDay<<endl;
+    cout<<"Final Total Pay predicted=$"<<totLDay<<endl;
+    //Exit
+    return 0;
+}
+
