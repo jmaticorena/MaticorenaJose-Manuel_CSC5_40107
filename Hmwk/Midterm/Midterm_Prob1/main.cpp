a831ddee3e11b7d7832b03f7f31f5f14451a654f
jmaticorena <j.maticorena@yahoo.com>
2017-01-24 22:13:01 -0800

    Signed-off-by: jmaticorena <j.maticorena@yahoo.com>

--- Hmwk/Midterm/Midterm_Prob1/main.cpp
+++ Hmwk/Midterm/Midterm_Prob1/main.cpp
@@ -5,35 +5,78 @@
  * Purpose: Display a Pattern Using a loop
  */
 
-//System Libraries Here
+//System Libraries
 #include <iostream>
 using namespace std;
 
-//User Libraries Here
+//User Libraries
 
-//Global Constants Only, No Global Variables
-//Like PI, e, Gravity, or conversions
+//Global Constants
+//Such as PI, Vc, -> Math/Science values
+//as well as conversions from system of units to another
 
-//Function Prototypes Here
+//Function Prototypes
 
-//Program Execution Begins Here
+
+//Executable code begins here!!!
 int main(int argc, char** argv) {
-    //Declare all Variables Here
-    unsigned int dim;//Dimensions of square
-    //Input
-    //cout<<"This program creates square matrix of"<<endl;
-    //cout<<"dimensions of your choosing."<<endl;
-    
-    cout<<"Input a positive Integer from 1-50"<<endl;
+    //Declare Variable
+    int dim;    //Number used to set dimensions
+    int NumMin; //Variable to keep track of decrementing number printed
+    int isEven; //Variable to determine if number is even
+    //Input 
+    cout<<"This program makes an X using the numbers"<<endl;
+    cout<<"from 1 to any number of your choosing"<<endl;
+    cout<<"Input any integer from 1-50"<<endl;
     cin>>dim;
-    //Processes of mapping inputs to outputs
-    for (int loop=1;loop<=dim;loop++){ //Loop for Rows
-        for (int X=1;X<=dim;X++){ //Loop for displaying X's
-            cout<<"x";
-        }
-        cout<<endl;
+    NumMin=dim; //Initializing to Number Inputed
+    
+    //Determining if number is even
+    isEven=dim%2; //Will return 0 or 1
+    
+    //Process of mapping Input to Outputs
+    switch(isEven){
+        case 0: //if number is even
+            for(int nRow=1;nRow<=dim;nRow++){     //loop for the rows
+                for(int nCol=1;nCol<=dim;nCol++){ //loop for the columns
+                    if (nRow==nCol||nCol==(dim+1)-nRow){ //Print out numbers
+                      if(nRow==nCol){ //Print out numbers in ascending order
+                          cout<<nRow;    
+                      }
+                      else{          //Print out numbers in descending order
+                          cout<<NumMin;
+                      }  
+                    }
+                    else {
+                        cout<<" "; //Print spaces
+                    }
+                }
+                NumMin--;  //Decrements the value
+                cout<<endl;
+            }
+            break;
+            
+        case 1://if number is odd
+            for(int nRow=1;nRow<=dim;nRow++){    //loop for the rows
+                for(int nCol=1;nCol<=dim;nCol++){//loop for the columns
+                    if (nRow==nCol||nCol==(dim+1)-nRow){//Print out numbers
+                      if(nRow==nCol){//Print out numbers in descending order
+                          cout<<NumMin;    
+                      }
+                      else{
+                          cout<<nRow;//Print out numbers in ascending order
+                      }  
+                    }
+                    else {
+                        cout<<" "; //Print spaces
+                    }
+                }
+                NumMin--;//Decrements the value
+                cout<<endl;
+            }
+            break;
     }
-    //Exit
+    
+    //Exit stage right!
     return 0;
 }
-
